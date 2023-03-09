53f5942140b0e9a068f65c6fd24370a0f4ce1880
Fernando <nndonick@gmail.com>
2023-03-09 18:32:50 +0700

    Payo_Bejalan Part2
    Melanjutkan pertemuan pertama

--- app/src/main/java/com/si61/payobejalan/TambahActivity.java
+++ app/src/main/java/com/si61/payobejalan/TambahActivity.java
@@ -1,14 +1,16 @@
 package com.si61.payobejalan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TambahActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tambah);
-    }
+        setContentView(R.layout.activity_tambah);    }
 }
